1ed941ea14a595b044cee9c72434f874a25c0539
islombek0995 <iabdulazizov425@gmail.com>
2022-12-09 17:27:22 +0500

    js_2vazifa

---
+++ main.js
@@ -0,0 +1,18 @@
+
+
+let baho = prompt("Ballingizni kiriting")
+if (baho >= 90){
+    console.log("Grant")
+}
+
+else if (baho <= 89 && baho >=70){
+    console.log("Kontrakt");
+}
+
+else if (baho <= 69 && baho >= 50){
+    console.log("SuperKontrakt");
+}
+
+else if (baho <= 50 <= 49 ){
+    console.log("Chopildingiz");
+}
